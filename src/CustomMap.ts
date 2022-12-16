fa9b959898627ed423e9b2fc01432a295f795bd6
Mihai Smarandache <mihai.sanfran@gmail.com>
2022-12-15 18:19:14 -0700

    Create interface for addMarker

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,6 +1,14 @@
 import { User } from "./User";
 import { Company } from "./Company";
 
+// Instructions to every other class on how they can be an argument to 'addMarker'
+interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+}
+
 export class CustomMap {
   private googleMap: google.maps.Map;
 
@@ -14,5 +22,5 @@ export class CustomMap {
     });
   }
 
-  addMarker(mappable: User | Company): void {}
+  addMarker(mappable: Mappable): void {}
 }
