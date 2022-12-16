ee6348dc932a0a41cffee6f96d5180e394b6a059
Mihai Smarandache <mihai.sanfran@gmail.com>
2022-12-15 19:50:10 -0700

    Export Mappable interface

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -2,7 +2,7 @@ import { User } from "./User";
 import { Company } from "./Company";
 
 // Instructions to every other class on how they can be an argument to 'addMarker'
-interface Mappable {
+export interface Mappable {
   location: {
     lat: number;
     lng: number;
